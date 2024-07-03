0852ff23d7eac80aef835db05f08df160277b105
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 13:41:24 +0900

    Delete one line in "when" block

--- Main.kt
+++ Main.kt
@@ -2,7 +2,6 @@ fun main() {
     val answer = 42
     when (answer - 1) {
         41 -> {
-            println("The answer is 42.")
             println(answer + 1)
         }
         else -> println("The answer is not 42.")
