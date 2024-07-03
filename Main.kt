8880126dcdec50e1afaa4851a08a156f3fc0b4b0
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:16:28 +0900

    Update "if" block

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    if (args.size == 0) {
-        println("Hello, World!");
+    if (args.size < 1) {
+        println("Hello, Kotlin!");
     } else {
         println("Hello, " + args[0] + "!");
     }
