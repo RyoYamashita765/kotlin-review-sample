64d5bafbc7c54d44c90538f4706eda6e3d396fb1
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:29:17 +0900

    Update "when" block

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    when (args.size) {
-        0 -> println("No arguments")
+    when (args.size + 1) {
+        1 -> println("No arguments")
         else -> {
             println("One or more arguments")
         }
