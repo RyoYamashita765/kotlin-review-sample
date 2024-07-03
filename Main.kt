6e133848389017eb35a3ce532c5d38996731b59f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:32:21 +0900

    Update multiple lines in "when" block

--- Main.kt
+++ Main.kt
@@ -2,9 +2,9 @@ fun main(args: Array<String>) {
     when (args.size + 1) {
         1 -> println("No arguments")
         else -> {
-            println("One or more arguments")
-            println("arguments size: ${args.size}")
-            println("arguments[0]: ${args[0]}")
+            println("Arguments:")
+            for (arg in args)
+                println(arg)
         }
     }
 }
