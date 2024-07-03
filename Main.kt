9c2fd54a7db28ce4197578a427b131e6db9045ed
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:24:03 +0900

    Update multiple lines in "for" block

--- Main.kt
+++ Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     for (i in 0..19) {
         println("Hello, World! [" + i + "]")
-        println("i = $i")
-        println("i * 2 = ${i * 2}")
+        println("i + 2 = ${i + 2}")
+        println("i * i = ${i * i}")
     }
 }
