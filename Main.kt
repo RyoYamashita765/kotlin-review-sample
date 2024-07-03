9dd855a55b322be0f785ae1110195c64f4b60c02
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 12:45:44 +0900

    Update one line in "if" block

--- Main.kt
+++ Main.kt
@@ -2,6 +2,6 @@ fun main() {
     val answer = 42
     if (answer >= 42) {
         println("The answer is " + answer)
-        println("The answer is greater than or equal to 42")
+        println("42 is less than or equal to the answer")
     }
 }
