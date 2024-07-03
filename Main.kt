2c3a0a22902b70feb5275888ae33b2e946c04a4b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:31:08 +0900

    Add multiple lines in "when" block

--- Main.kt
+++ Main.kt
@@ -3,6 +3,8 @@ fun main(args: Array<String>) {
         1 -> println("No arguments")
         else -> {
             println("One or more arguments")
+            println("arguments size: ${args.size}")
+            println("arguments[0]: ${args[0]}")
         }
     }
 }
