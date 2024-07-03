96e3ece12199fc01f531eb253a4f9614adc6c7ed
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 13:44:16 +0900

    Update one line in "while" block

--- Main.kt
+++ Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     var answer = 42
     while (answer >= 0) {
-        println("Hello, World!")
+        println("Hello, Kotlin World!")
         println("The answer is $answer")
         answer--
     }
