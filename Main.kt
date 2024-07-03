b9f81eac46c010786c2123bc100b321732adfc69
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:07:36 +0900

    Delete multiple lines

--- Main.kt
+++ Main.kt
@@ -1,5 +1,3 @@
 fun main() {
-    println("Quo usque tandem abutere, Catilina, patientia nostra?")
-    println("Quam diu etiam furor iste tuus nos eludet?")
-    println("Quem ad finem sese effrenata iactabit audacia?")
+
 }
