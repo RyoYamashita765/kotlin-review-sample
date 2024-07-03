a64d33e26b64a3f2a143dff1b66cb1c5fd4f5941
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:42:51 +0900

    Delete multiple lines in "while" block

--- Main.kt
+++ Main.kt
@@ -2,8 +2,6 @@ fun main(args: Array<String>) {
     var answer = 0
     while (answer <= 42) {
         println("The answer:" + answer)
-        answer += 4
-        answer++
         answer++
     }
 }
