b10f502a573f46b67fa857b44d72cba2fdc65167
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:24:20 +0900

    Delete multiple lines in "for" block

--- Main.kt
+++ Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
     for (i in 0..19) {
-        println("Hello, World! [" + i + "]")
-        println("i + 2 = ${i + 2}")
         println("i * i = ${i * i}")
     }
 }
