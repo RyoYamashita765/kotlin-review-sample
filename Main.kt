4502feca8a1aa2be96291f17d76c4718a4cb2f45
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 18:41:07 +0900

    Update "while" block

--- Main.kt
+++ Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     var answer = 0
-    while (answer != 42) {
-        println("answer: " + answer)
+    while (answer <= 42) {
+        println("The answer is " + answer)
         answer++
     }
 }
