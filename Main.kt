663a8e8ab0a02a507f894643634305ba5ec6ef82
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 11:34:53 +0900

    Add sample case: "Think twice to use a guard clause when your logic has nested if-else statements."

--- Main.kt
+++ Main.kt
@@ -1,7 +1,18 @@
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val arr = arrayOf(1, 2, 3)
-        println(arr[4])
+        if (args.size > 0) {
+            if (args.size > 1) {
+                if (args.size > 2) {
+                    println("Too many arguments")
+                } else {
+                    println("Two arguments")
+                }
+            } else {
+                println("One argument")
+            }
+        } else {
+            println("No arguments")
+        }
     }
 }
