36988c697b3ad54ab4007d32e8aac72d3d4a0d14
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 11:35:11 +0900

    Remove codes

--- Main.kt
+++ Main.kt
@@ -1,18 +1,6 @@
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        if (args.size > 0) {
-            if (args.size > 1) {
-                if (args.size > 2) {
-                    println("Too many arguments")
-                } else {
-                    println("Two arguments")
-                }
-            } else {
-                println("One argument")
-            }
-        } else {
-            println("No arguments")
-        }
+
     }
 }
