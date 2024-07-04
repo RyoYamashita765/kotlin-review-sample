71d0b3278c3680440c5fa20e6dedeca2bee4989b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:20:41 +0900

    Add "when" block

--- Main.kt
+++ Main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    when (args.size) {
+        0 -> {
+            println("No arguments")
+        }
+        1 -> {
+            println("One argument")
+        }
+        else -> {
+            println("More than one argument")
+        }
+    }
 }
