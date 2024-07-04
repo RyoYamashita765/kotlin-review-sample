8e312bfec109c3f786e89b99799defd26e10d52d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:33:44 +0900

    Add multiple lines to some points

--- Main.kt
+++ Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
+    println("Arguments: ${args.joinToString()}")
+    println("Environment: ${System.getenv().map { (k, v) -> "$k=$v" }.joinToString(", ")}")
+    println("Properties: ${System.getProperties().map { (k, v) -> "$k=$v" }.joinToString(", ")}")
+
     println("Hello World!")
+
+    val name = "kotlin"
+    println("Hello $name!")
+    println("Hello ${name.uppercase()}!")
 }
