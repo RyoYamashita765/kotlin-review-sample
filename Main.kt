346ae0eb40082fb7be5c07144f3622115f057019
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 11:33:10 +0900

    Add sample case: "Validate if variables are always safe to avoid Internal Server Error"

--- Main.kt
+++ Main.kt
@@ -1,12 +1,7 @@
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        var result = ""
-        if (args.size == 0) {
-            result = "Hello, World!"
-        } else {
-            result = "Hello, ${args[0]}!"
-        }
-        println(result)
+        val arr = arrayOf(1, 2, 3)
+        println(arr[4])
     }
 }
