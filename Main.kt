2d868182e7820de077724ab415e24fbfc16f7718
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:35:03 +0900

    Delete multiple lines to some points

--- Main.kt
+++ Main.kt
@@ -1,11 +1,3 @@
 fun main(args: Array<String>) {
-    println("Arguments: ${args.joinToString()}")
-    println("Environments: ${System.getenv().size}")
-    println("Properties: ${System.getProperties().size}")
-
     println("Hello World!")
-
-    val name = "kotlin"
-    println("Hello, $name!")
-    println("Hello, ${name.uppercase()}!")
 }
