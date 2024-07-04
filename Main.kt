8466387375188fe0be80f95370b8c7b1b2a96e1b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:23:26 +0900

    Delete "when" block

--- Main.kt
+++ Main.kt
@@ -1,15 +1,3 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    when (args.size + 1) {
-        0 -> {
-            println("No arguments")
-        }
-        2 -> {
-            println("One argument")
-        }
-        else -> {
-            println("More than one argument")
-        }
-    }
 }
