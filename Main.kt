d7a93271fb446a9d4333c8cd7a5bfdd5062cf702
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:17:36 +0900

    Delete lines in "for" block

--- Main.kt
+++ Main.kt
@@ -2,8 +2,6 @@ fun main(args: Array<String>) {
     println("Hello World!")
 
     for (i in 1..(args.size)) {
-        println("----- Arguments -----")
         println("Argument $i: ${args[i - 1]}")
-        println("---------------------")
     }
 }
