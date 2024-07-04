d5863b68168e55883719e39247229cf45aeabf87
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:09:41 +0900

    Add lines to some points

--- Main.kt
+++ Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
+    println("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 
+
+
+    println("Hello World!")
+
+
+
+    println("Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
 }
