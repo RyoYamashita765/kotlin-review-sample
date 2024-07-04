eb86adb688656596fd11cc66e86733440f83ca13
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:10:30 +0900

    Delete lines from some points

--- Main.kt
+++ Main.kt
@@ -1,11 +1,3 @@
 fun main(args: Array<String>) {
-    println("Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
-
-
-
     println("Hello World!")
-
-
-
-    println("Duis aute irure dolor in reprehenderit in voluptate ...")
 }
