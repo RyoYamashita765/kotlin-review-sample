0c89da9acd54b8e5a4b0a3a2be9211da8465affc
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 10:12:01 +0900

    Update "if" block

--- Main.kt
+++ Main.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    if (args.size > 0) {
+    if (args.size >= 1) {
         println("Args: ${args.size}")
-        println("Hello, ${args[0]}!")
+        println("Hello, [${args[0]}]!")
     }
 }
