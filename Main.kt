27efa0f318d99c20c76af3b7a267a171b24a4781
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-04 11:37:12 +0900

    Add sample case: "import statements should be the alphabetical order."

--- Main.kt
+++ Main.kt
@@ -1,6 +1,16 @@
+import java.util.HashMap
+import java.util.ArrayList
+
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
+        val list = ArrayList<String>()
+        list.add("Hello")
+        list.add("World")
+        list.add("!")
 
+        val map = HashMap<String, String>()
+        map["Hello"] = "World"
+        map["Kotlin"] = "Java"
     }
 }
