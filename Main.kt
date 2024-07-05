ad7b19a1612e14353ecac4495380d2d23d4ada18
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-05 10:40:08 +0900

    Add sample case: "MUST avoid hard-coding."

--- Main.kt
+++ Main.kt
@@ -2,9 +2,9 @@ public class Main {
     companion object {
         @JvmStatic
         fun main() {
-            println("Hello, World!")
-
-            println("Arguments:" + args.joinToString())
+            val circleRadius = 5.0
+            val circleArea = 3.141592 * circleRadius * circleRadius
+            println("Circle area is $circleArea")
         }
     }
 }
