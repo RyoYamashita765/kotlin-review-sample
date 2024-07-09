c5d132992f25d0d550bd7efa6cb49aacff256a75
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-09 12:02:34 +0900

    Remove codes

--- Main.kt
+++ Main.kt
@@ -4,13 +4,6 @@ import java.util.ArrayList
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val list = ArrayList<String>()
-        list.add("Hello")
-        list.add("World")
-        list.add("!")
 
-        val map = HashMap<String, String>()
-        map["Hello"] = "World"
-        map["Kotlin"] = "Java"
     }
 }
