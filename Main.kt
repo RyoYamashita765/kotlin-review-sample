44f0bfc3a2ec59b5bcfa0afb7ced81f88d640df3
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-09 12:20:59 +0900

    Add sample case: "Do NOT use inheritance as possible, use composition instead."

--- Main.kt
+++ Main.kt
@@ -1,9 +1,30 @@
-import java.util.HashMap
-import java.util.ArrayList
+open class Logger {
+    fun log(message: String) {
+        println("Logging: $message")
+    }
+}
+
+class FileLogger : Logger() {
+    fun saveToFile(fileName: String) {
+        println("Saving to file: $fileName")
+    }
+}
+
+class NetworkLogger : Logger() {
+    fun sendToServer(url: String) {
+        println("Sending to server: $url")
+    }
+}
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
+        val fileLogger = FileLogger()
+        fileLogger.log("File log message")
+        fileLogger.saveToFile("log.txt")
 
+        val networkLogger = NetworkLogger()
+        networkLogger.log("Network log message")
+        networkLogger.sendToServer("http://example.com")
     }
 }
