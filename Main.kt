3b4965a7e0c5bf69f04c6108e622effcd5cc39f1
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-09 14:24:10 +0900

    Add sample case: "Write your code without `else` statements where possible."

--- Main.kt
+++ Main.kt
@@ -1,19 +1,15 @@
-class UserService(private val userRepository: UserRepository) {
-
-    fun getUserById(id: Long) = userRepository.findById(id)
-
-    fun createUser(name: String, email: String) {
-        val user = User(name = name, email = email)
-        userRepository.save(user)
-    }
-
-    fun updateUserEmail(id: Long, newEmail: String) =
-        userRepository.findById(id)?.let { user ->
-            user.email = newEmail
-            userRepository.save(user)
+fun getDiscountPercentage(customer: Customer): Int {
+    if (customer.isPremium) {
+        if (customer.loyaltyYears > 5) {
+            return 20
+        } else {
+            return 15
         }
-
-    fun deleteUser(id: Long) = userRepository.deleteById(id)
-
-    fun searchUsers(query: String) = userRepository.search(query)
+    } else {
+        if (customer.purchaseCount > 10) {
+            return 10
+        } else {
+            return 5
+        }
+    }
 }
