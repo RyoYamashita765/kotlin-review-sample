35af08457f709131307ed9123c92fd457160476e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-11 15:18:46 +0900

    Add sample case: Data class functionality should be used.

--- Main.kt
+++ Main.kt
@@ -1,23 +1,10 @@
 public class Main {
-    val siteUrl = "https://www.exmaple.com"
-    private val userRepository = UserRepository()
-
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val circleRadius = 5.0
-            val circleArea = 3.141592 * circleRadius * circleRadius
-            println("Circle area is $circleArea")
+
         }
     }
 }
 
-class UserRepository {
-    fun processUserProfile(profile: UserProfile) {
-        println(profile.name.length)
-    }
-}
-
-class UserProfile {
-    var name: String? = null
-}
+class User(val id: String, val name: String)
